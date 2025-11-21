e310ac9fbc9686cd1f7fea16bbb5b6285482fa27
dexterchan <dexterchan@gmail.com>
2025-11-21 13:18:35 +0800

    adj longhorn setup

--- install-longhorn.sh
+++ install-longhorn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update && sudo apt install open-iscsi -y
-
+sudo apt-get install nfs-common -y
 
 kubectl label nodes rasp-node1 rasp-node2 rasp-node3 storage=longhorn
 
@@ -8,3 +8,5 @@ helm repo add longhorn https://charts.longhorn.io
 helm repo update
 
 helm install longhorn longhorn/longhorn -n longhorn-system --create-namespace --values long-horn-storage-values.yaml
+
+kubectl apply -f long-horn-ingress.yaml
