d5def27c4a0839ec6342315b6b12d89a3458f77b
dexterchan <dexterchan@gmail.com>
2025-11-30 21:04:13 +0800

    tempo collapse server

--- k3s/observatory/grafana-loki-helm.sh
+++ k3s/observatory/grafana-loki-helm.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # https://grafana.com/docs/loki/latest/setup/install/helm/install-monolithic/
-#helm repo add grafana https://grafana.github.io/helm-charts
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update
 
 #helm show values grafana/loki > loki-values.yaml
 
