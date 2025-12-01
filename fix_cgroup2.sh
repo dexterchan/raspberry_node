0e4d9786bb3d9bb22848840f6003cb703d1fdcb0
dexterchan <dexterchan@gmail.com>
2025-12-01 19:02:58 +0800

    fix dns

--- fix_cgroup2.sh
+++ fix_cgroup2.sh
@@ -8,6 +8,10 @@ echo $(cat /boot/firmware/cmdline.txt) csystemd.unified_cgroup_hierarchy=1 cgrou
 sudo reboot
 cat /sys/fs/cgroup/cgroup.controllers
 
+#fix dns server
+echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf
+echo "nameserver 1.1.1.1" | sudo tee -a /etc/resolv.conf
+
 #Failed to find memory cgroup, you may need to add "cgroup_memory=1 cgroup_enable=memory" to your linux cmdline (/boot/firmware/cmdline.txt on a Raspberry Pi)
 #[INFO]  systemd: Enabling k3s-agent unit
 #    sudo systemctl status k3s-agent.services
